94c7eda68b3054f85af34ce1c06e4893fa94f61e
Thalisson. <126502431+Thalisson-DEV@users.noreply.github.com>
2025-12-28 12:36:08 -0300

    fix: cors incompatibillity

--- backend/src/main/java/com/portifolio/controller/EmailController.java
+++ backend/src/main/java/com/portifolio/controller/EmailController.java
@@ -12,7 +12,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/email")
-@CrossOrigin(origins = "http://localhost:4200")
 public class EmailController {
 
     private final EmailService emailService;
